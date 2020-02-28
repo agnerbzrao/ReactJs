d49301d3e7d96ad0d2930a199badb11ad4df264c
Agner Souza Bezerra <agner.bezerra@pravaler-030.idealinvest.local>
2020-02-28 15:22:56 -0300

    adicionando

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+<Main icon="home" title="Início" subtitle="Segundo Projeto">
+<div className="display-4">Bem vindo!</div>
+<p className="mb-0">Sistema em ReactJs</p>
+</Main>
--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -2,6 +2,9 @@ import './Footer.css'
 import React from 'react'
 
 export default props =>
-<footer className="footer">
-    Rodapé
-</footer>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Agner</strong>
+        </span>
+    </footer>
